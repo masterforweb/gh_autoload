5540d73a1b9c8f85303575483afcbfdf5eb7e1d1
akdelf <ak.delf@gmail.com>
2012-07-22 23:20:01 +0400

    add autoload function

--- autoload.php
+++ autoload.php
@@ -2,9 +2,11 @@
 
 	
 	# папка где буду лежать библиотека
-	define('LIBPATH', $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATION.'libs');
+	define('LIBPATH', 'libs/');
 
 	
 	# подгружаемые библиотеки c github
 	gh_download('hottags', 'akdelf');
-	gh_download('fAK', 'akdelf');
+	//gh_download('fAK', 'akdelf');
+
+	echo charset('UTF-8');
--- gh_download.php
+++ gh_download.php
@@ -12,24 +12,38 @@ else
 
 
 
-function gh_download($repo, $user) {
 
 
-    $file = 'http://github.com/'.$user.'/'.$repo.'/zipball/master';
-    $work_dir = $repo.'_work';
+function gh_download($user, $repo, $sfile = null, $branch = 'master') {
+
+    $repo_dir = LIBPATH.$repo; //директория где будет установлен пакет
+    
+    if ($sfile == null)
+        $sfile = $repo.'.php';    
+      
+    $sfile = $repo_dir.'/'.$sfile;
+
+    if (MODE !== 'update' and file_exists($sfile)) {
+        require($sfile); // автозагрузка библиотеки
+        return True;
+    }
+
+    $file = 'http://github.com/'.$user.'/'.$repo.'/zipball/'.$branch; //получаем файл для занрузки
+    $repo_dir = LIBPATH.$repo; //директория где будет установлен пакет
+    $work_dir = $repo_dir.'_work'; // директория для распаковки
     $newfile = LIBPATH.$repo.'.zip';
 
-    if (MODE == 'init' and is_dir($work_dir)) //файл скачен
+    if (MODE == 'init' and is_dir($work_dir)) //файл уже скачен
         return;
-    
+
+
     if (!copy($file, $newfile)) {
         echo "не удалось скопировать $file...\n";
         return;
     }else {
         echo $file." импортирован... \n";
     }    
-
-    $work_dir = $repo.'_work';
+   
     $zip = new ZipArchive;
     $res = $zip->open($newfile);
     if ($res === TRUE) {
@@ -38,8 +52,8 @@ function gh_download($repo, $user) {
         echo $newfile." успешно разархивирован... \n"; 
         $files = scandir($work_dir);
         foreach ($files as $file) {
-            if (strpos($file, $user.'-'.$repo) !== False) {
-                rename($work_dir.DIRECTORY_SEPARATOR.$file.DIRECTORY_SEPARATOR, $repo);
+            if (strpos($file, $user.'-'.$repo) !== False) { // ищем папку с последним коммитом
+                rename($work_dir.'/'.$file.'/', $repo_dir);
                 removedir($work_dir);
                 break;
             }    
@@ -48,15 +62,19 @@ function gh_download($repo, $user) {
 
     unlink($newfile);
 
+    require($sfile);
+    
+
     return;
 }
- 
+
+
+
 
 function removedir($dir) {
     
     if ($files = glob($dir.'/*')) {
        
-       print_r($files);
        foreach($files as $file) {
          
          if (is_dir($file))
